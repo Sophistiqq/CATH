e1274ef1a8c28d201678ebebd41c9651e7fd6b87
roi.for.school@gmail.com <roi.for.school@gmail.com>
2022-12-19 09:05:41 +0800

    LAST

--- firebase.js
+++ firebase.js
@@ -2,50 +2,50 @@
 firebase.initializeApp(firebaseConfig);
 
 firebase.auth().onAuthStateChanged(function(user) {
-  if (user) {
-    // User is signed in. Show the admin page.
-  } else {
-    // No user is signed in. Redirect to the login page.
-    if (window.location.pathname !== '/loginpage.html') {
-    	window.location.replace("loginpage.html");
-    }
-  }
+	if (user) {
+		// User is signed in. Show the admin page.
+	} else {
+		// No user is signed in. Redirect to the login page.
+		if (window.location.pathname !== '/loginpage.html') {
+			window.location.replace("loginpage.html");
+		}
+	}
 });
 
 const form = document.querySelector(".form");
 
 form.addEventListener("submit", function(event) {
-  event.preventDefault();
+	event.preventDefault();
 
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
+	const email = form.elements.email.value;
+	const password = form.elements.password.value;
 
-  // Validate email and password
-  if (!isValidEmail(email)) {
-    // Display an error message
-    return;
-  }
-  if (!isValidPassword(password)) {
-    // Display an error message
-    return;
-  }
+	// Validate email and password
+	if (!isValidEmail(email)) {
+		// Display an error message
+		return;
+	}
+	if (!isValidPassword(password)) {
+		// Display an error message
+		return;
+	}
 
-  firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
-    // User logged in successfully
-    window.location.replace("adminpage.html");
-  }).catch(function(error) {
-    // An error occurred while trying to log in
-    console.error(error.message);
-    // Display an error message to the user
-  });
+	firebase.auth().signInWithEmailAndPassword(email, password).then(function() {
+		// User logged in successfully
+		window.location.replace("adminpage.html");
+	}).catch(function(error) {
+		// An error occurred while trying to log in
+		console.error(error.message);
+		// Display an error message to the user
+	});
 });
 
 function isValidEmail(email) {
-  // Add your email validation logic here
-  return true;
+	// Add your email validation logic here
+	return true;
 }
 
 function isValidPassword(password) {
-  // Add your password validation logic here
-  return true;
+	// Add your password validation logic here
+	return true;
 }
